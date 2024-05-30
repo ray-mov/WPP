debda6c2dda90f064442c1288e9481d3004941c1
ray-mov <monovray@gmail.com>
2024-05-31 00:30:31 +0530

    assignment 8

---
+++ 08.js
@@ -0,0 +1,10 @@
+//Write a JavaScript program to check whether a string starts with 'Java' if it does not otherwise.
+
+
+
+function isStartWithJava(str){
+  console.log(str.startsWith("Java"))
+}
+
+isStartWithJava("JavaScript")
+isStartWithJava("ScriptJava")
